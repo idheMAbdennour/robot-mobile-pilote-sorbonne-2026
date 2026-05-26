0948addf5d7c7b431ef906452d7a5b344a7dd2d8
jpnodev <jpnodev@gmail.com>
2026-05-26 15:23:56 +0200

    fix(ir): 0V au repos, on active et désactive la sortie

--- robot/src/emissionIR.c
+++ robot/src/emissionIR.c
@@ -9,23 +9,19 @@
 #define TIMER_PRESCALER 25 // Pour obtenir une résolution de 1us
 
 
-// PWM 38kHz sur P1.21
+// PWM 38kHz sur P1.22
 #define PCLK_PWM (25000000) // Si l'horloge periphérique est à 25MHz
 #define FREQ_IR 38000
 #define PWM_PRESCALER 25 // Pour obtenir une résolution de 1us
-#define PWM_PERIOD PCLK_PWM / ((PWM_PRESCALER) * FREQ_IR) // Nombre de ticks pour 38kHz
+#define PWM_PERIOD (PCLK_PWM / (PWM_PRESCALER * FREQ_IR)) // Nombre de ticks pour 38kHz
 
-// Broche P1.21
-#define PIN_MASK (1 << 21)
+// Broche P1.22
+#define PIN_MASK (1 << 22)
 
-volatile uint8_t bob = 0;
 void init_PWM_IR(void) {
-	
-	LPC_SC->PCONP |= (1 << 6);
-  
-	// 1. Définir le P1.21 en mode PWM1.3
-	LPC_PINCON->PINSEL3 &= ~(3 << 10); // Clear bits pour P1.21
-	LPC_PINCON->PINSEL3 |= (2 << 10);  // Fixer P1.21 à la fonction PWM1.3 (0b10)
+    // 1. Définir le P1.22 en mode PWM1.3
+	LPC_PINCON->PINSEL3 &= ~(3 << 12); // Clear bits pour P1.22
+	LPC_PINCON->PINSEL3 |= (2 << 12);  // Fixer P1.22 à la fonction PWM1.3 (0b10)
 
 	// 2. Configurer le PWM1.3 pour une fréquence de 38kHz
 	LPC_SC->PCLKSEL0 &= ~(3 << 12); // PCLK_PWM1 = CCLK (25MHz)
@@ -41,33 +37,32 @@ void init_PWM_IR(void) {
 	// 4. Latch des registres MR0 et MR3
 	LPC_PWM1->LER = (1 << 0) | (1 << 3);
 
-	// 5. Comportement lorsque le Timer atteint MR0 (période) : Reset et coninuer
-	LPC_PWM1->MCR &= ~(1 << 0); // Pas d'interruption sur MR0
-	LPC_PWM1->MCR |= (1 << 1); // Reset on MR0
-	LPC_PWM1->MCR &= ~(1 << 2); // Pas de stop sur MR0
+	// // 5. Comportement lorsque le Timer atteint MR0 (période) : Reset et coninuer
+	// LPC_PWM1->MCR &= ~(1 << 0); // Pas d'interruption sur MR0
+	// LPC_PWM1->MCR |= (1 << 1); // Reset on MR0
+	// LPC_PWM1->MCR &= ~(1 << 2); // Pas de stop sur MR0
 
-	// 6. Configurer la sortie activée, mais compteur à l'arrêt et reseté par défaut
-  LPC_PWM1->PCR |= (1 << 11);          // Enable PWM3 output (reste connectée à P1.22)
+	// // 6. Configurer la sortie activée, mais compteur à l'arrêt et reseté par défaut
+    // LPC_PWM1->PCR |= (1 << 11);          // Enable PWM3 output (reste connectée à P1.22)
 
 
 	// LPC_PWM1->TCR &= ~(1 << 0); // Counter OFF (0)
 	// LPC_PWM1->TCR |= (1 << 1) | (1 << 3); // Reset ON (1), PWM Mode (3)
 	// LPC_PWM1->TCR &= ~(1 << 1); // Reset OFF (0)
-	
-	// Reset du compteur
-	LPC_PWM1->TCR = (1 << 1);
 
-	// Activer PWM mode + compteur
-	LPC_PWM1->TCR = (1 << 3) | (1 << 0);
-	
-	// Interrupt sur MR0
-	LPC_PWM1->MCR |= (1 << 0);   // Interrupt on MR3
-	
-	// Interrupt sur MR3
-	LPC_PWM1->MCR |= (1 << 9);   // Interrupt on MR3
+    // 5. Comportement lorsque le Timer atteint MR0 (période) : Reset et coninuer
+    LPC_PWM1->MCR &= ~(1 << 0);
+    LPC_PWM1->MCR |= (1 << 1);
+    LPC_PWM1->MCR &= ~(1 << 2);
+
+    // 6. Configurer la sortie activée, mais compteur à l'arrêt et reseté par défaut
+    LPC_PWM1->PCR &= ~(1 << 11);
+
+    // 6. On active le mode PWM et on laisse le compteur tourner librement
+    LPC_PWM1->TCR = (1 << 3) | (1 << 0);
 }
 
-void init_Timer_Enveloppe(uint16_t delai_us) {
+void init_Timer_Enveloppe(uint16_t délai_us) {
     // 1. Allumer le Timer 0
     LPC_SC->PCONP |= (1 << 1);
 
@@ -76,7 +71,7 @@ void init_Timer_Enveloppe(uint16_t delai_us) {
 
     // 3. Configuration du Timer pour 250us (un temps 't')
     LPC_TIM0->PR = TIMER_PRESCALER - 1; // Prescaler pour obtenir 1us
-    LPC_TIM0->MR0 = delai_us - 1; // ticks = délai_us
+    LPC_TIM0->MR0 = délai_us - 1; // ticks = délai_us
 
     // 4. Reset & Interrupt sur Match 0
     LPC_TIM0->MCR |= (1 << 0) | (1 << 1);
@@ -85,6 +80,7 @@ void init_Timer_Enveloppe(uint16_t delai_us) {
     // 5. Activer l'interruption au niveau du NVIC
     NVIC_EnableIRQ(TIMER0_IRQn);
 }
+
 
 // Interruption déclenchée toutes les 250us (un temps 't')
 void TIMER0_IRQHandler(void) {
@@ -99,10 +95,10 @@ void TIMER0_IRQHandler(void) {
 
 // Sous-routine pour gérer l'état du matériel PWM à chaque temps 't'
 void update_PWM_state(void) {
+
     // PHASE BLANC LORS DES CYCLES 3, 4 et 5 (Silence entre les rafales)
     if (frame_counter >= 3) {
-        LPC_PWM1->TCR &= ~(1 << 0); // Counter OFF
-		LPC_PWM1->TCR |= (1 << 1); // Reset ON
+        LPC_PWM1->PCR &= ~(1 << 11); // Désactiver la sortie -> 0V
 
         seq_index++;
         if (seq_index >= seq_length) {
@@ -110,24 +106,23 @@ void update_PWM_state(void) {
             frame_counter++;
             if (frame_counter >= 6) {
                 frame_counter = 0; // On reprend un nouveau cycle d'émission, reconstruire la trame :
-                preparer_trame(get_robot_number(), get_robot_vitesse(), (uint8_t)get_robot_status());
+                preparer_trame(get_robot_number(), get_robot_vitesse(), (uint8_t)get_robot_ir_status());
             }
         }
         return;
     }
 
     // PHASE D'ÉMISSION LORS DES CYCLES 0, 1, 2
-    if (ir_sequence[seq_index] == 1) {
-        // Émettre le train d'impulsion : On enlève le reset et on lance le compteur
-        LPC_PWM1->TCR |= (1 << 1); // Reset ON
-				LPC_PWM1->TCR |= (1 << 0); // Counter ON
-				LPC_PWM1->TCR &= ~(1 << 1); // Reset OFF
-				// LPC_PWM1->TCR = (1 << 3) | (1 << 0);
-		} else {
-        // Blanc (repos à 0V) : On reset/coupe le compteur
-        LPC_PWM1->TCR &= ~(1 << 0); // Counter OFF
-				// LPC_PWM1->TCR = (1 << 3) | (1 << 1);
-		LPC_PWM1->TCR |= (1 << 1); // Reset ON
+if (ir_sequence[seq_index] == 1) {
+        // Reset le compteur pour synchroniser la phase à 38kHz
+        LPC_PWM1->TCR |= (1 << 1);  // Reset ON
+        LPC_PWM1->TCR &= ~(1 << 1); // Reset OFF
+
+        // Activer la sortie
+        LPC_PWM1->PCR |= (1 << 11);
+    } else {
+        // Blanc : Désactiver la sortie
+        LPC_PWM1->PCR &= ~(1 << 11);
     }
 
     // Passage au temps 't' suivant
@@ -137,8 +132,7 @@ void update_PWM_state(void) {
         seq_index = 0;
         frame_counter++;
 
-		LPC_PWM1->TCR |= (1 << 1); // Reset ON
-		LPC_PWM1->TCR &= ~(1 << 0); // Counter OFF
+		LPC_PWM1->PCR &= ~(1 << 11);
     }
 }
 
