e186bb7116006c6f8f3c160975e1bb3264c84134
khoviadin442 <kitiho975@gmail.com>
2026-05-29 17:16:51 +0200

    mise a jour

--- robot/src/main.c
+++ robot/src/main.c
@@ -11,8 +11,6 @@
 
 volatile uint8_t flag_50hz = 0;
 
-
-
 int main(void)
 {
     SystemInit();
@@ -23,19 +21,12 @@ int main(void)
     init_moteurs_debug();
     init_proximetre();
     init_capteur_inductif();
-    Init_Moteur_PWM();
 
     SysTick_Config(SystemCoreClock / 50);
     set_debug_uart_enabled(1);
 
-    uint32_t last_time = 0;
-    uint8_t speed = 70;
-    uint8_t state = 0;
-
     while (1)
     {
-        Changer_PWM_Gauche(speed);
-        Changer_PWM_Droite(speed);
         if (!flag_50hz)
         {
             continue;
@@ -43,31 +34,17 @@ int main(void)
 
         flag_50hz = 0;
 
+        // Balayage non bloquant du proximètre : doit tourner même si le debug UART
+        // est désactivé. La trame "T/t ddd..." est émise à la fin de chaque balayage
+        // depuis proximetre_tick() (gardée par get_debug_uart_enabled()).
+        proximetre_tick();
+
         if (!get_debug_uart_enabled()) {
             continue;
         }
 
         // Envois debug par module
-        //debug_moteurs_send_frame();
-        //debug_inductif_send_frame();
-
-        // Note : Ne PAS mettre debug_proximetre_send_frame() ici !
-        // Le cahier des charges dit "à chaque balayage".
-        // Il faudra l'appeler à la fin de la fonction qui gère le servo-moteur du proximètre.
+        debug_moteurs_send_frame();
+        debug_inductif_send_frame();
     }
 }
-/*
-int main(void) {
-    SysTick_Config(SystemCoreClock / 1000);
-    
-    Init_Moteur_PWM();
-    
-    uint32_t last_time = 0;
-    uint8_t speed = 70;
-    uint8_t state = 0;
-
-    while(1) {
-				Changer_PWM_Gauche(speed);
-        Changer_PWM_Droite(speed);
-    }
-} */
